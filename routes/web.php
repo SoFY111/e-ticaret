e9c25330f8e2993705ad2edad3190a421bc6fa5a
Hakan DİNÇTÜRK <hakan.dincturkk@gmail.com>
2022-03-01 19:38:13 +0300

    template implement 1/3

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('front.index');
+});
+
+Route::get('/ver', function () {
+    return view('ver');
 });
