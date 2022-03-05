bd35d46f8f46e6b70e2cc80fedf7d2bfc3b239ca
unknown <cengiz69mg@gmail.com>
2022-03-05 15:53:56 +0300

    win,xbox,ps slider

--- resources/views/front/layouts/footer.blade.php
+++ resources/views/front/layouts/footer.blade.php
@@ -187,6 +187,26 @@
 </script>
 <!-- //here ends scrolling icon -->
 
+<script src="{{asset('front/js/responsiveslides.min.js')}}"></script>
+				<script>
+						// You can also use "$(window).load(function() {"
+						$(function () {
+						 // Slideshow 4
+						$("#slider3").responsiveSlides({
+							auto: true,
+							pager: true,
+							nav: false,
+							speed: 500,
+							namespace: "callbacks",
+							before: function () {
+						$('.events').append("<li>before event fired.</li>");
+						},
+						after: function () {
+							$('.events').append("<li>after event fired.</li>");
+							}
+							});
+						});
+</script>
 
 <!-- for bootstrap working -->
 <script type="text/javascript" src="{{asset('front/js/bootstrap.js')}}"></script>
